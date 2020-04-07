ef93529d20ed022abff858fa55f252a7d99c1158
cdiasant <cdias002@odu.edu>
2020-04-07 17:29:02 -0400

    Added Skeleton function to populate Virtual File System

---
+++ src/edu/odu/cs/cs471/VirtualFileSystem/PopulateVFS.java
@@ -0,0 +1,23 @@
+package edu.odu.cs.cs471.VirtualFileSystem;
+/**
+ * Child class of Virtual System
+ * populate File System
+ * @author Chris
+ *
+ */
+public class PopulateVFS extends VirtualSystem {
+	
+	public static VirtualSystem populate() {
+		VirtualSystem System = new VirtualSystem();
+		
+		//TODO Prepopulate a Virtual System
+		
+		
+		
+		
+		return System;
+	}
+	
+	//TODO Find way to use a tree TOO.
+
+}
