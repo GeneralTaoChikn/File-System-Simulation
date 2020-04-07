e828d8c1ba4cb505b6189747a5e95f5d3d3c65d6
cdiasant <cdias002@odu.edu>
2020-04-07 16:15:41 -0400

    Idea for Gui

--- src/edu/odu/cs/cs471/Main.java
+++ src/edu/odu/cs/cs471/Main.java
@@ -3,10 +3,15 @@ package edu.odu.cs.cs471;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JTree;
+import javax.swing.JTextArea;
+
+//import edu.odu.cs.cs471.VirtualFileSystem.*;	
 
 public class Main {
 
 	private JFrame frame;
+	//VirtualSystem system = new VirtualSystem();
 
 	/**
 	 * Launch the application.
@@ -36,8 +41,16 @@ public class Main {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 1000, 675);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
+		
+		JTree tree = new JTree();
+		tree.setBounds(12, 13, 312, 419);
+		frame.getContentPane().add(tree);
+		
+		JTextArea textArea = new JTextArea();
+		textArea.setBounds(516, 24, 351, 351);
+		frame.getContentPane().add(textArea);
 	}
-
 }
